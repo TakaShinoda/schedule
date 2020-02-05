24697a7af4c82c26e4440a998fb1250fd388670c
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-05 17:37:30 +0900

    タイトルをレスポンシブに対応

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import MediaQuery from "react-responsive";
+
 
 const Home = () => {
     return (
         <div>
-            <h1>Home画面</h1>
+            <div className="title">
+                {/**PC画面サイズ */}
+                <MediaQuery query="(min-width: 768px)">
+                  <div className="max-width">
+                    <h1>Schedule</h1>
+                </div>
+                </MediaQuery>
+                {/**スマホ画面サイズ */}
+                <MediaQuery query="(max-width: 767px)">
+                    <div className="min-width">
+                        <h1>Schedule</h1>
+                    </div>
+                </MediaQuery>
+                
+            </div>
         </div>
     );
 }
