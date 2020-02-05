7b437d7ccdbd120b36d122061a20cbbf72e2d2da
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-05 20:51:15 +0900

    fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import Trello from './Trello';
 import Home from './Home';
 
-const Menu = () => {
+
+
+
+
+class Menu extends Component {
+    constructor(props){
+        super(props);
+        this.state =({
+
+        });
+        //this.signInWithGoogle = this.signInWithGoogle.bind(this);
+    }
+    /** 
+
+    signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(user => {
+            alert("success : " + user.user.displayName + "さんでログインしました");
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+    }
+
+    signOut() {
+        //firebase.auth().signOut();
+        alert("hege")
+    }
+
+*/
+
+    render() {
     return (
         <div className="navbar-header">
         <Router>
                 <ul className="navbar">
                     <div className="navbar-left">
                         <li><Link to='/'>ホーム</Link></li>
-                        <li><Link to='./trello' onClick={()=>alert('hoge')}>管理</Link></li>
+                        <li><Link to='./trello' /*onClick={()=>{this.signInWithGoogle()}*}*/>管理する</Link></li>
                     </div>
                     <div className="navbar-right">
                         <li>ログアウト</li>
@@ -25,6 +59,7 @@ const Menu = () => {
 
    
     );
+}
 }
 
 export default Menu;
