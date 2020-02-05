da237ae123e3a1932d6eedf771524a445042aed8
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-05 20:51:46 +0900

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Menu />
+
+    </div>
+  );
+}
+
+export default App;
