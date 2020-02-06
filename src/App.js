3f118fc5ced5eb067b00f287d6394faff6593c76
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-07 02:13:29 +0900

    Firebaseの設定を追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Menu from './components/Menu';
 import './App.css';
+import firebase from 'firebase/app';
+import "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
      <Menu />
-
     </div>
   );
 }
