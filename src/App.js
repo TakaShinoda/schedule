d7d301e2eb8cb7c53fd5bbbacf05ac64f0a73dc1
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-07 17:24:45 +0900

    firebaseConfigの修正

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ import "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(firebaseConfig);
+
+
 
 function App() {
   return (
     <div className="App">
      <Menu />
+     {console.log(process.env.REACT_APP_FIREBASE_API_KEY)}
+     {console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN)}
+     {console.log(process.env.REACT_APP_FIREBASE_DATABASE_URL)}
+     {console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID)}
+     {console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET)}
+     {console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID)}
+     {console.log(process.env.REACT_APP_FIREBASE_APP_ID)}
+     {console.log(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID)}
     </div>
   );
 }
