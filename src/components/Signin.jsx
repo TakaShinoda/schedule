68ac3ec43ce8327dbc81a9c8230407b66ab32e95
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-07 17:10:36 +0900

    googleアカウントでのログインの実装

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,38 +1,35 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import firebase from 'firebase/app';
 
 
-class Signin extends Component {
-    constructor(props){
-        super(props);
-        this.signInWithGoogle = this.signInWithGoogle.bind(this);
-    }
-
-    signInWithGoogle(e) {
-        alert("hoge")
+const Signin = () => {
+    
+    const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider()
         firebase
         .auth()
         .signInWithPopup(provider)
-        .then(result => {
-            console.log(result);
+        .then(user => {
+            alert("success : " + user.user.displayName + "さんでログインしました");
+            //this.$router.push("/tourlist");
+          })
+          .catch(error => {
+              alert(error.message);
           });
     }
-
-    render() {
-        return (
+    
+    
+    return (
             <div>
                 <div className="login">
                     <h1>ログイン / 新規登録</h1>
                 </div>
                 <div className="signin_button">
-                    <img src="../btn_google_signin.png" onClick={()=>this.signInWithGoogle()} alt="google signin"/>
+                    <img src="../btn_google_signin.png" onClick={()=>signInWithGoogle()} alt="google signin"/>
                 </div>
             </div>
         );
-    }
-
 }
 
 export default Signin
