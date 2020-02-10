763cd532fd04a4fa4ef6e9b0947e4086b93b9a01
Takahiro <tkhr_621@docomo.ne.jp>
2020-02-10 23:55:20 +0900

    fixed

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -2,17 +2,19 @@
 import React from 'react';
 import firebase from 'firebase/app';
 
-
 const Signin = () => {
-    
+
     const signInWithGoogle = () => {
+        //Googleプロバイダオブジェクトのインスタンスを作成
         const provider = new firebase.auth.GoogleAuthProvider()
+        //ポップアップウィンドウでログインを行う場合はsignInWithPopupを呼び出す
         firebase
         .auth()
         .signInWithPopup(provider)
         .then(user => {
             alert("success : " + user.user.displayName + "さんでログインしました");
-            //this.$router.push("/tourlist");
+            //リダイレクト処理
+            
           })
           .catch(error => {
               alert(error.message);
@@ -23,7 +25,7 @@ const Signin = () => {
     return (
             <div>
                 <div className="login">
-                    <h1>ログイン / 新規登録</h1>
+                    <h1>ログイン</h1>
                 </div>
                 <div className="signin_button">
                     <img src="../btn_google_signin.png" onClick={()=>signInWithGoogle()} alt="google signin"/>
